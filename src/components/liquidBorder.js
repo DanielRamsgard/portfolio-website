dc6eab9caade1159537e21042e7ff33ef7f8a91a
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-07-03 15:29:23 -0400

    initial commit

---
+++ src/components/liquidBorder.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LiquidBorder = () => {
+    return (
+        <>
+            <div className="land-image-container">
+                <img className="land-image" alt="Daniel Ramsgard" src="/static/media/img.jpeg"></img>
+            </div>  
+        </>
+    );
+}
+
+export default LiquidBorder;
