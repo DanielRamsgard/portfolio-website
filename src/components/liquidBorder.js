9d1bff9b49c24ee3a72e506f813a8cf557189a4a
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-07-03 21:31:16 -0400

    work

--- src/components/liquidBorder.js
+++ src/components/liquidBorder.js
@@ -4,7 +4,7 @@ const LiquidBorder = () => {
     return (
         <>
             <div className="land-image-container">
-                <img className="land-image" alt="Daniel Ramsgard" src="/static/media/img.jpeg"></img>
+                <img className="land-image" alt="Daniel Ramsgard" src="/static/media/img.jpeg" draggable="false"></img>
             </div>  
         </>
     );
